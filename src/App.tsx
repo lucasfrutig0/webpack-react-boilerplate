33bfdc2e9c031df5e9309e194b765cc9c223a21c
lucas <lucasfrutig0@gmal.com>
2022-12-29 16:31:11 -0300

    initial commit 2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export const App = () => {
   return (
     <>
       <h1>
-        React Template - {process.env.NODE_ENV} {process.env.name}
+        Webpack React Template - {process.env.NODE_ENV} {process.env.name}
       </h1>
       <img src={IMAGE} alt="react logo" width="180px" height="auto" />
       <img src={LOGO} alt="react logo" width="300px" />
